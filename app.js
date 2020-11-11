db3b570e341d17b40ea966412fa3a27ca3e0f6f9
Ivan Zhuravlev <i.zhuravlev@gmail.com>
2020-11-11 04:45:07 +0300

    server been start

--- app.js
+++ app.js
@@ -1,1 +1,9 @@
-console.log("app")
+const express = require("express")
+const config = require("config")
+
+const app = express()
+
+const PORT = config.get("port") || 5000
+
+app.listen(5000, () => console.log(`App has been started on port ${PORT}...`))
+
